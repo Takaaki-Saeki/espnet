1a61f6a98a0c1e26e22c6d16515aa31711dadf6e
Takaaki-Saeki <saefrospace@gmail.com>
2022-11-17 01:25:11 +0900

    add vits recipe

---
+++ egs2/masmultts/tts_vits_byte_cm1/run.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+fs=16000
+n_fft=1024
+n_shift=256
+
+################# Configs to be set #####################
+token_type=byte
+use_mailabs=true
+use_css10=true
+use_fleurs=false
+use_lid=true
+mos_filtering=true
+lang_set=null
+do_trimming=false
+#########################################################
+
+local_data_opts=""
+local_data_opts+=" --token_type ${token_type}"
+local_data_opts+=" --use_mailabs ${use_mailabs}"
+local_data_opes+=" --use_css10 ${use_css10}"
+local_data_opts+=" --use_fleurs ${use_fleurs}"
+local_data_opts+=" --mos_filtering ${mos_filtering}"
+local_data_opts+=" --lang_set ${lang_set}"
+local_data_opts+=" --do_trimming ${do_trimming}"
+
+opts=
+if [ "${fs}" -eq 22050 ]; then
+    # To suppress recreation, specify wav format
+    opts="--audio_format wav "
+else
+    opts="--audio_format flac "
+fi
+
+lang=noinfo
+cleaner=none
+if [ ${token_type} = "byte" ]; then
+    model_token_type=byte
+    g2p=byte
+elif [ ${token_type} = "tphn" ]; then
+    model_token_type=char
+    g2p=none
+else
+    echo "Error: token_type must be either byte or tphn"
+    exit 1
+fi
+
+train_config=conf/train.yaml
+inference_config=conf/decode.yaml
+
+train_set=train
+valid_set=dev
+test_sets=test
+
+./tts.sh \
+    --lang ${lang} \
+    --local_data_opts "${local_data_opts}" \
+    --feats_type raw \
+    --use_lid ${use_lid} \
+    --fs "${fs}" \
+    --n_fft "${n_fft}" \
+    --n_shift "${n_shift}" \
+    --win_length null \
+    --use_xvector true \
+    --xvector_tool rawnet \
+    --token_type "${model_token_type}" \
+    --cleaner "${cleaner}" \
+    --g2p "${g2p}" \
+    --tts_task gan_tts \
+    --feats_extract linear_spectrogram \
+    --feats_normalize none \
+    --train_config "${train_config}" \
+    --inference_config "${inference_config}" \
+    --inference_model latest.pth \
+    --min_wav_duration 0.1 \
+    --max_wav_duration 15 \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --srctexts "data/${train_set}/text" \
+    ${opts} "$@"

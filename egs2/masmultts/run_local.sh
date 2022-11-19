3c783fa34eb11d28344422301b9d853316e01bf3
Takaaki-Saeki <saefrospace@gmail.com>
2022-11-20 08:46:42 +0900

    fix

--- egs2/masmultts/run_local.sh
+++ egs2/masmultts/run_local.sh
@@ -1,17 +1,22 @@
 #############################
-name="tts_pre_byte_m_en"
+name="tts_byte_c"
 token="byte"
+train_args=""
+
+# If using fine-tuning
+train_args="--init_param ../tts_pre_byte_m_en/exp/tts_train_raw_byte/train.loss.best.pth:::tts.lid_emb"
 #############################
 
 datadir="data"
 dumpdir="dump"
 
+# Run preprocessing
 cd ${name}
-./run.sh --stage 3 --stop-stage 4
+#./run.sh --stage 1 --stop-stage 4
+
 # change token list
-rm -rf ${dumpdir}/token_list/${token}/tokens.txt
-cd ${dumpdir}/token_list/${token}
-ln -s ../../../../token_list_${token}.txt tokens.txt
-cd ../../../
+#rm -rf ${dumpdir}/token_list/${token}/tokens.txt
+#cp ../token_list_${token}.txt ${dumpdir}/token_list/${token}/tokens.txt
+
 # run training
-./run.sh --stage 5 --stop-stage 6 --ngpu 1
+./run.sh --stage 5 --stop-stage 6 --ngpu 1 --train_args "${train_args}"

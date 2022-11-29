429a83df7b3bdcc346d0c7179c0f5caeb0985c13
Takaaki-Saeki <saefrospace@gmail.com>
2022-11-29 10:02:33 +0900

    fix

--- egs2/masmultts/run_local.sh
+++ egs2/masmultts/run_local.sh
@@ -1,22 +1,47 @@
 #############################
-name="tts_byte_1L_es"
+name="tts_byte_hq13_mlm"
 token="byte"
-train_args=""
-
-# If using fine-tuning
-train_args="--init_param ../tts_pre_byte_m_en/exp/tts_train_raw_byte/valid.loss.best.pth:::tts.lid_emb"
+use_mlm=true
 #############################
 
 datadir="data"
 dumpdir="dump"
 
+if [ ${use_mlm} = true ]; then
+    cp conf/train.yaml conf/train_override.yaml
+fi
+
+
 # Run preprocessing
 cd ${name}
-#./run.sh --stage 1 --stop-stage 4
+./run.sh --stage 1 --stop-stage 4
+
+if [ ${use_mlm} = true ]; then
+    python3 local/write_holdout_info.py \
+	    --lang_set lang_set.txt \
+	    --holdout_lang holdout_lang_set.txt \
+	    --org_dir ${dumpdir}/raw/org \
+	    --config conf/train.yaml
+fi
 
 # change token list
-#rm -rf ${dumpdir}/token_list/${token}/tokens.txt
-#cp ../token_list_${token}.txt ${dumpdir}/token_list/${token}/tokens.txt
+if [ ${token} = "tphn" ]; then
+    token_name="char"
+elif [ ${token} = "phn" ]; then
+    token_name="char"
+elif [ ${token} = "byte" ]; then
+    token_name="byte"
+elif [ ${token} = "bphn" ]; then
+    token_name="word"
+else
+    echo "No such token: ${token}"
+    exit 1
+fi
+
+train_args="--init_param ../${pretrain_name}/exp/tts_train_raw_${token_name}/valid.loss.best.pth:::tts.lid_emb"
+
+rm -rf ${dumpdir}/token_list/${token_name}/tokens.txt
+cp ../token_list_${token}.txt ${dumpdir}/token_list/${token_name}/tokens.txt
 
 # run training
 ./run.sh --stage 5 --stop-stage 6 --ngpu 1 --train_args "${train_args}"

6ca557f1ed143b1a1803a464238ebf736da73d2d
Takaaki-Saeki <saefrospace@gmail.com>
2022-12-16 01:06:29 +0900

    fix

--- egs2/masmultts/tts_vits_byte_css10/run.sh
+++ egs2/masmultts/tts_vits_byte_css10/run.sh
@@ -10,7 +10,7 @@ n_fft=1024
 n_shift=256
 
 ################# Configs to be set #####################
-token_type=byte   # byte, tphn, phn, bphn
+token_type=byte           # byte, tphn, phn, bphn
 use_mailabs=false
 use_css10=true
 use_fleurs=false
@@ -93,16 +93,18 @@ test_sets=test
     --fs "${fs}" \
     --n_fft "${n_fft}" \
     --n_shift "${n_shift}" \
+    --win_length null \
     --use_xvector true \
     --xvector_tool speechbrain \
-    --use_lvector ${use_lvector} \
-    --lvector_feats_type fam \
     --token_type "${model_token_type}" \
     --cleaner "${cleaner}" \
     --g2p "${g2p}" \
+    --tts_task gan_tts \
+    --feats_extract linear_spectrogram \
+    --feats_normalize none \
     --train_config "${train_config}" \
     --inference_config "${inference_config}" \
-    --inference_model valid.loss.best.pth \
+    --inference_model latest.pth \
     --min_wav_duration 0.1 \
     --max_wav_duration 15 \
     --train_set "${train_set}" \
